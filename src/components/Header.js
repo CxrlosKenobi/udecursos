ed0d60b1b4209727e2448fb130395e9772ef7efa
CxrlosKenobi <yolitopinto@gmail.com>
2021-11-12 00:54:56 -0300

    Update fully responsive!!

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,10 @@
 import '../css/Header.css';
 import logo from '../assets/logo.png';
-import styled from 'styled-components';
 
-const H3 = styled.h3`
-  font-family: "Suisse Int\'l Mono", monospace;
-  font-size: 12px;
-  font-weight: bold;
-  color: #10162F;
-
-`;
 
 const Header = () => {
-    return (
-        <header className="Header">
-            <div className="HeaderContent">
+    const BurgerNav = () => {
+        return (
             <nav id="navigation">
                 <div id="menu-toggle">
                     <input type="checkbox" />
@@ -22,39 +13,43 @@ const Header = () => {
                     <span></span>
                     <ul id="menu">
                         {/* <p><a href="http://secad.ing.udec.cl/horarios">Udec 2021-2</a></p> */}
-                        <li><a href="/Home">Home</a></li>
+                        <li><a href="/Inicio">Inicio</a></li>
                         <li><a href="/Malla">Malla</a></li>
                     </ul>
                 </div>
             </nav>
-                <div className="presentation">
-                <div id='img-container'>
-                    <a href="/">
-                    <img src={logo} alt="logo" height="70" width="52"/>
-                    </a>
-                </div>
-                <div id="heading-container">
-                    <h1>UdeCursos</h1>
-                </div>
-                </div>
+        );
+    }
+    
 
-                <div className="RightHeader">
-                <h3>
-                    <a 
-                    href="http://www.inf.udec.cl/"
-                    target="_blank" rel="noopener noreferrer"
-                    >
-                    <H3>Ingeniería Civil Informática</H3>
-                    </a>
-                </h3>
-                <h3>
-                    <a 
-                    href="http://secad.ing.udec.cl/horarios"
-                    target="_blank" rel="noopener noreferrer"
-                    >
-                    <H3>UdeC 2021-2</H3>
-                    </a>
-                </h3>
+    return (
+        <header className="Header">
+            <div className="HeaderContent">
+                <div className="presentation">
+                    <div id='img-container'>
+                        <a href="/">
+                            <img src={logo} alt="logo" height="70" width="52"/>
+                        </a>
+                    </div>
+                    <div id="heading-container">
+                        <h1>UdeCursos</h1>
+                    </div>
+                </div>
+                <BurgerNav />
+                <div id="RightHeader">
+                    <h3>
+                        <a href="http://www.inf.udec.cl/" 
+                            target="_blank" rel="noopener noreferrer">
+                            Ingeniería Civil Informática
+                        </a>
+                    </h3>
+                    <h3>
+                        <a href="http://secad.ing.udec.cl/horarios"
+                            target="_blank" rel="noopener noreferrer"
+                        >
+                            UdeC 2021-2
+                        </a>
+                    </h3>
                 </div>
             </div>
         </header>
