c599de8c117260cdb89e565e19c9ca3d0634fc50
CxrlosKenobi <kenobi@ieee.org>
2022-03-07 04:53:37 -0300

    Update enhanced responsive and migration to Sass

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,20 +4,20 @@ import './index.scss'
 export default function Footer(){
   return (
     <footer>
-      <div className="content">
-        <div className="column-1">
+      <div className="footer-wrapper">
+        <div className="last-update">
           <a href='https://github.com/CxrlosKenobi/udecursos/commits/master' 
-            target="_blank" rel="noopener noreferrer" className="last-update">
-            Última actualización:<br></br>Febrero 7, 2022
+            target="_blank" rel="noopener noreferrer">
+            Última actualización:<br></br>Marzo 7, 2022
           </a>
         </div>
-        <div className="column-2">
+        <div className="source-code">
           <a href="https://github.com/CxrlosKenobi/udecursos"
             target="_blank" rel="noopener noreferrer">
             <i className="fab fa-github"></i>   Código fuente
           </a>
         </div>
-        <div className="column-3">
+        <div className="signature">
           <a href='https://github.com/CxrlosKenobi'>
             <i className="fas fa-code"></i>   with   ❤️   by Kenobi
           </a>
