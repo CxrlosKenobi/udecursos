3a4fb5851cdc28bfb86dddb8225b67e57aefa3dc
CxrlosKenobi <kenobi@ieee.org>
2022-03-07 04:52:18 -0300

    Update to self component and enhanced code structure

---
+++ src/views/Inicio/GitHubAPI.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from "react";
+import { Octokit } from '@octokit/core';
+//
+import { token } from '../../data/octok.json';
+//
+import "./GitHubAPI.scss";
+
+
+export function CommitList() {
+	const [received, setReceived] = useState(false);
+	const [changelogs, setChangelogs] = useState(null);
+	const [commits, setCommits] = useState([]);
+	
+	useEffect(() => {
+		const octokit = new Octokit({ auth: token })
+		async function fetchAPI() {
+			const owner = 'CxrlosKenobi', repo = 'udecursos'
+
+			try {
+				const response = await octokit.request(
+					`GET /repos/{owner}/{repo}/commits`, 
+					{ owner, repo, per_page: 5 }
+				);
+				if (response.status === 200) {
+					setCommits(response)
+					setReceived(true)
+				} else {
+					console.log("Couldn't fetch data from the GitHub API :(")
+					setReceived(false)
+				}
+			} catch(e) {
+				console.log(e)
+			}
+		}
+		fetchAPI()
+	}, [])
+	
+	// Aux function for this case.
+	function parseDate(date) {
+		let y = date.split('T')[0].split('-')[0]
+		let m = date.split('T')[0].split('-')[1]
+		let d = date.split('T')[0].split('-')[2]
+
+		return `${d}/${m}/${y}`
+	}
+
+	useEffect(() => {
+		if (received) {
+			setChangelogs(	
+				commits.data.map(request => {
+					return (
+						<div key={request.sha} className="commit">
+							<div className='profilepic-container'>
+								<img src={request.author.avatar_url} alt='GitHub profile pic' />
+							</div>
+							<div className='commit-info'>
+								<a href={request.author.html_url} className='_login' alt={request.commit.author.name}
+                  target="_blank" rel="noopener noreferrer">
+                  @{request.author.login}
+								</a>
+							</div>
+							<div className='commit-desc'>
+								<a href={request.html_url} className='_desc' target="_blank" rel="noopener noreferrer">
+                  {request.commit.message}
+								</a>
+							</div>
+							<div className='_date'>
+								<h2>{parseDate(request.commit.author.date)}</h2>
+							</div>
+						</div>
+					);
+				})
+			)
+		} else {
+			setChangelogs(
+        <div className='changelogs-error'>
+          <p>Obteniendo datos de la API . . .</p>
+        </div>
+			)
+		}
+	}, [received, commits])
+	
+  return changelogs
+};
