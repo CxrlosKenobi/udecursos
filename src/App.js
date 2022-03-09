19da5d28ca2f5f2a56926e9556fb8e9fa24693ff
CxrlosKenobi <kenobi@ieee.org>
2022-03-08 21:56:23 -0300

    Add redux store for persistent data on localStorage!

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
+import React from "react";
+import store from "./state/store";
+import { Provider } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 //
-import Header from './components/Header/index';
-import { Inicio } from './views/Inicio/index';
-import { Malla } from './views/Malla/index';
-import { Utilidades } from './views/Utilidades/index';
-import Footer from './components/Footer/index';
+import Header from "./components/Header/index";
+import { Inicio } from "./views/Inicio/index";
+import { Malla } from "./views/Malla/index";
+import { Utilidades } from "./views/Utilidades/index";
+import Footer from "./components/Footer/index";
 //
-import '@atlaskit/css-reset';
-import './App.css';
+import "@atlaskit/css-reset";
+import "./App.css";
 
 
 export default function UdeCursos() {
   return (
-    <section id='UdeCursos'>
-      <BrowserRouter>
-        <Header/>
-        <Routes>
-          <Route exact path='/' element={<Inicio/>} />
-          <Route path="/Malla" element={<Malla/>} />
-          <Route path="/Utilidades" element={<Utilidades/>} />
-        </Routes>
-        <Footer/>
-      </BrowserRouter>
-    </section>
+    <Provider store={store}>
+      <section id="UdeCursos">
+        <BrowserRouter>
+          <Header/>
+          <Routes>
+            <Route exact path="/" element={<Inicio/>} />
+            <Route path="/Malla" element={<Malla/>} />
+            <Route path="/Utilidades" element={<Utilidades/>} />
+          </Routes>
+          <Footer/>
+        </BrowserRouter>
+      </section>
+    </Provider>
   );
 };
--- src/state/cartSlice.js
+++ src/state/cartSlice.js
@@ -1,5 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-export const initialState = {
-  carrera: ""
-}
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    career: {},
+  },
+
+  reducers: {
+    sendCareer: (state, action)  => {
+      state.career = action.payload;
+    }
+  }
+});
+
+export const { sendCareer } = cartSlice.actions;
+export const cartSelector = state => state.cart;
+export default cartSlice.reducer;
