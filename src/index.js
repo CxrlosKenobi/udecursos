00ab932e32557895b92667d77f3404c86ca304b3
CxrlosKenobi <carpinto2021@inf.udec.cl>
2022-08-16 22:56:56 -0400

    Upgrading packages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import UdeCursos from './App';
-import './index.css';
+import './index.scss';
 
-ReactDOM.render(
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
       <UdeCursos />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
